025e1baacf54829229306c627f94c04e6d825e42
Chris Bordeman <cbordeman48@gmail.com>
2025-08-11 04:17:12 -0400

    Cleanup: Add summary to IOnViewLoaded interface

--- Biz.Core/IOnViewLoaded.cs
+++ Biz.Core/IOnViewLoaded.cs
@@ -1,5 +1,9 @@
 ﻿namespace Biz.Core;
 
+/// <summary>
+/// When used with UserControlEx (or invoked on your view's OnLoaded(),
+/// provides way to execute code after the view has been fully loaded.
+/// </summary>
 public interface IOnViewLoaded
 {
     void OnViewLoaded();
