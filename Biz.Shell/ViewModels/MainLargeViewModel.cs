12f8beeb91d115d6946aee8af6e7537c0918a1ff
Chris Bordeman <cbordeman48@gmail.com>
2025-08-14 16:49:00 -0400

    Refactor & Enhance: Replace static sidebar content with dynamic ItemsControl binding, update MainLargeViewModel with SidebarItems property, and reformat related XAML for consistency.

--- Biz.Shell/ViewModels/MainLargeViewModel.cs
+++ Biz.Shell/ViewModels/MainLargeViewModel.cs
@@ -6,6 +6,15 @@ namespace Biz.Shell.ViewModels;
 
 public class MainLargeViewModel : MainViewModelBase
 {
+    #region SidebarItems
+    public ObservableCollection<SidebarHeaderViewModel> SidebarItems
+    {
+        get => sidebarItems;
+        set => SetProperty(ref sidebarItems, value);
+    }
+    ObservableCollection<SidebarHeaderViewModel> sidebarItems;        
+    #endregion SidebarItems
+    
     #region Greeting
     string greeting = "Welcome to Shell (Large)";
     public string Greeting
